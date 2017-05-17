1c865eeebd40a7868cacea355f644423f71f158b
TwitchGG <twitchgg@yahoo.com>
2017-05-17 17:04:52 +0800

    Initial project

---
+++ sources/api/restfully-server/src/main/java/com/anyun/cloud/demo/api/node/Main.java
@@ -0,0 +1,11 @@
+package com.anyun.cloud.demo.api.node;
+
+/**
+ * @auth TwitchGG <twitchgg@yahoo.com>
+ * @since 1.0.0 on 2017/5/16
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
