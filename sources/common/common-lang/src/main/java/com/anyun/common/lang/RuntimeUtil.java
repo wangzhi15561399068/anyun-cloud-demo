75c549bbefffb75bed90da3ca2188238c912990e
TwitchGG <twitchgg@yahoo.com>
2017-05-19 09:56:12 +0800

    Add anyuncloud common library

---
+++ sources/common/common-lang/src/main/java/com/anyun/common/lang/RuntimeUtil.java
@@ -0,0 +1,36 @@
+/*
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.anyun.common.lang;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.RuntimeMXBean;
+
+/**
+ * @author ray
+ */
+public class RuntimeUtil {
+
+    public static int getPid() {
+        RuntimeMXBean runtime = ManagementFactory.getRuntimeMXBean();
+        String name = runtime.getName();
+        int index = name.indexOf("@");
+        int pid = 0;
+        if (index != -1) {
+            pid = Integer.parseInt(name.substring(0, index));
+        }
+        return pid;
+    }
+}
